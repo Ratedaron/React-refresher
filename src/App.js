3335e5e2f03814cff4a252312b3539817df634ba
Aron Haubner <aron.haubner@immopac.ch>
2024-12-03 14:36:57 +0100

    Destructuring v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 
-const myArray = ['Apple', 'Melon', 'Frutituti', 'Cornflakes'];
-const myArrayOfHeaders = ['Welcome', 'to', 'my', 'World'];
+const vehicles = ['mustang', 'Ferari', 'f-150', 'f-40'];
+//const myArrayOfHeaders = ['Welcome', 'to', 'my', 'World'];
 
-const myList = myArray.map((item, index) => <p key={index}>{item}</p>);
-const myIndexedList = myArrayOfHeaders.map((item, index) => <p key={index}>{item}</p>);
+const [car, sportsCar, truck, plane] = vehicles
+
+const [car2, sportsCar2,, plane2 ] = vehicles
 
 function App() {
   return (
     <div className="App">
-      <h1 className="index">{myIndexedList}</h1>
-      <h1>{myList}</h1>
+      <h1>{car}</h1>
+      <h1>{sportsCar}</h1>
+      <h1>{truck}</h1>
+      <h1>{plane}</h1>
+      <br />
+      <h1>And these are the Shorter ones</h1>
+      <h1>{car2}</h1>
+      <h1>{sportsCar2}</h1>
+      <h1>{plane2}</h1>
     </div>
   );
 }
