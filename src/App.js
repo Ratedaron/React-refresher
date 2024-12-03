c405e3a19e2e4e90a04c6f129af0a155aa26d3e9
Aron Haubner <aron.haubner@immopac.ch>
2024-12-03 16:50:15 +0100

    Expressions and ifs in a JSX/ JSXML elements

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
 
-const person = {
-  hobbies: ["reading", "hiking", "coding"],
-  lastName: "Doe",
-  address: {
-    street: "123 Main St",
-    city: "New York",
-    state: "NY",
-    zipCode: "10001"
-  },
-  age: 30,
-  isEmployed: true,
-  firstName: "John",
-  greet: function () {
-    return `Hello, my name is ${this.firstName} ${this.lastName}.`;
-  }
-};
-
-function returnPersonInfo({ firstName, lastName, age, hobbies, address: { street } }) {
-  return `This person is called ${firstName} ${lastName}, is ${age} years old, likes to do ${hobbies[0]}, and lives on ${street}.`;
-}
-
+const x = 9
 function App() {
-  const message = returnPersonInfo(person);
-
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1>Quick Math: 5+5 is {5 + 5}</h1>
+      <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
     </div>
   );
 }
