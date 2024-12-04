599efd4a449a56364b12ddf1fc62be28a2e9d320
Aron Haubner <aron.haubner@immopac.ch>
2024-12-04 12:35:24 +0100

    First onClick event (event)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
-import Car from './Car.js'
 
-function Car1() {
-  return (
-    <h1>Hi I am a Car</h1>
-  );
+function football() {
+  const shoot = (a, b) => {
+    alert(a + ' + ' + b.currentTarget)
+  }
+
+  return (<button id="button" onClick={(event) => shoot("GOALLLL", event)}>Shooott</button>)
 }
 
-const car = { name: "Ford", type: "FastCar" }
 
-function App(para) {
-  const carName = "Ford MUSSTANG"
+function App() {
   return (
     <div className="App">
-      <h1>Who Is in this Gradge??</h1>
-      <Car1 />
-      <Car brand={car} />
-      <br />
-      <h1>I'm driving a {para.color} car</h1>
+      {football()}
     </div>
   );
 }
--- src/Car.js
+++
@@ -1,7 +0,0 @@
-function Car(props) {
-    return (
-        <h2>Hi, I am A {props.brand.name} / {props.brand.type} from a diffrewnt file</h2>
-    );
-}
-
-export default Car;
